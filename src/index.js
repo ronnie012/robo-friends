1db3e953569dd3ae2c3ccca38007aded25542a17
ronnie012 <ronnie.247365.1@gmail.com>
2021-12-12 21:05:33 +0600

    Built robo-friends React App.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
+import { robots } from './robots';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(
+								<CardList robots={robots}/>
+, document.getElementById('root'));
+registerServiceWorker();
